e917f5c4746be6112ce98a457695759f220d13ce
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2024-06-09 22:21:06 -0300

    initialize repository

---
+++ Library.Api/Data/DatabaseInitializer.cs
@@ -0,0 +1,21 @@
+﻿using Dapper;
+
+namespace Library.Api.Data;
+
+public class DatabaseInitializer(IDbConnectionFactory connectionFactory)
+{
+    public async Task InitializeAsync()
+    {
+        using var connection = await connectionFactory.CreateConnectionAsync();
+        await connection.ExecuteAsync("""
+                                      CREATE TABLE IF NOT EXISTS Books (
+                                          Isbn TEXT PRIMARY KEY,
+                                          Title TEXT NOT NULL,
+                                          Author TEXT NOT NULL,
+                                          ShortDescription TEXT NOT NULL,
+                                          PageCount INTEGER NOT NULL,
+                                          ReleaseDate TEXT NOT NULL
+                                      );
+                                      """);
+    }
+}
---
+++ Library.Api/Data/SqliteConnectionFactory.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using Microsoft.Data.Sqlite;
+
+namespace Library.Api.Data;
+
+public class SqliteConnectionFactory(string connectionString) : IDbConnectionFactory
+{
+    public async Task<IDbConnection> CreateConnectionAsync()
+    {
+        var connection = new SqliteConnection(connectionString);
+        await connection.OpenAsync();
+        return connection;
+    }
+}
