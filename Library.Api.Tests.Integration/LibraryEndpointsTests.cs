301c7416b3cd962b03aa5d86db28617831c748ae
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2024-06-10 16:54:33 -0300

    testing resource deletion

--- Library.Api.Tests.Integration/LibraryEndpointsTests.cs
+++ Library.Api.Tests.Integration/LibraryEndpointsTests.cs
@@ -10,14 +10,14 @@ public class LibraryEndpointsTests(WebApplicationFactory<IApiMarker> factory)
     : IClassFixture<WebApplicationFactory<IApiMarker>>, IAsyncLifetime
 {
     private readonly List<string> _createdIsbns = [];
-    
+
     [Fact]
     public async void CreateBook_CreatesBook_WhenDataIsCorrect()
     {
         // Arrange
         var httpClient = factory.CreateClient();
         var book = GenerateBook();
-        
+
         // Act
         var result = await httpClient.PostAsJsonAsync("/books", book);
         _createdIsbns.Add(book.Isbn);
@@ -36,7 +36,7 @@ public class LibraryEndpointsTests(WebApplicationFactory<IApiMarker> factory)
         var httpClient = factory.CreateClient();
         var book = GenerateBook();
         book.Isbn = "Invalid ISBN";
-        
+
         // Act
         var result = await httpClient.PostAsJsonAsync("/books", book);
         _createdIsbns.Add(book.Isbn);
@@ -48,15 +48,14 @@ public class LibraryEndpointsTests(WebApplicationFactory<IApiMarker> factory)
         error.PropertyName.Should().Be("Isbn");
         error.ErrorMessage.Should().Be("Invalid ISBN format");
     }
-    
+
     [Fact]
     public async void CreateBook_Fails_WhenBookExists()
     {
         // Arrange
         var httpClient = factory.CreateClient();
         var book = GenerateBook();
-        book.Isbn = "Invalid ISBN";
-        
+
         // Act
         await httpClient.PostAsJsonAsync("/books", book);
         _createdIsbns.Add(book.Isbn);
@@ -78,26 +77,26 @@ public class LibraryEndpointsTests(WebApplicationFactory<IApiMarker> factory)
         var book = GenerateBook();
         await httpClient.PostAsJsonAsync("/books", book);
         _createdIsbns.Add(book.Isbn);
-        
+
         // Act
         var result = await httpClient.GetAsync($"/books/{book.Isbn}");
         var existingBook = await result.Content.ReadFromJsonAsync<Book>();
-        
+
         // Assert
         result.StatusCode.Should().Be(HttpStatusCode.OK);
         existingBook.Should().BeEquivalentTo(book);
     }
-    
+
     [Fact]
     public async Task GetBook_ReturnsNotFound_WhenBookDoesNotExist()
     {
         // Arrange
         var httpClient = factory.CreateClient();
         var isbn = GenerateIsbn();
-        
+
         // Act
         var result = await httpClient.GetAsync($"/books/{isbn}");
-        
+
         // Assert
         result.StatusCode.Should().Be(HttpStatusCode.NotFound);
     }
@@ -111,26 +110,26 @@ public class LibraryEndpointsTests(WebApplicationFactory<IApiMarker> factory)
         await httpClient.PostAsJsonAsync("/books", book);
         _createdIsbns.Add(book.Isbn);
         var books = new List<Book> { book };
-        
+
         // Act
         var result = await httpClient.GetAsync("/books");
         var existingBooks = await result.Content.ReadFromJsonAsync<List<Book>>();
-        
+
         // Assert
         result.StatusCode.Should().Be(HttpStatusCode.OK);
         existingBooks.Should().BeEquivalentTo(books);
     }
-    
+
     [Fact]
     public async Task GetAllBooks_ReturnsEmptyList_WhenNoBooksExist()
     {
         // Arrange
         var httpClient = factory.CreateClient();
-        
+
         // Act
         var result = await httpClient.GetAsync("/books");
         var returnedBooks = await result.Content.ReadFromJsonAsync<List<Book>>();
-        
+
         // Assert
         result.StatusCode.Should().Be(HttpStatusCode.OK);
         returnedBooks.Should().BeEmpty();
@@ -145,14 +144,102 @@ public class LibraryEndpointsTests(WebApplicationFactory<IApiMarker> factory)
         await httpClient.PostAsJsonAsync("/books", book);
         _createdIsbns.Add(book.Isbn);
         var books = new List<Book> { book };
-        
+
         // Act
         var result = await httpClient.GetAsync($"/books?searchTerm=est");
         var existingBooks = await result.Content.ReadFromJsonAsync<List<Book>>();
-        
+
         // Assert
         result.StatusCode.Should().Be(HttpStatusCode.OK);
         existingBooks.Should().BeEquivalentTo(books);
+    }
+
+    [Fact]
+    public async Task SearchBooks_ReturnsEmptyList_WhenTitleDoesNotMatch()
+    {
+        // Arrange
+        var httpClient = factory.CreateClient();
+        var book = GenerateBook();
+        await httpClient.PostAsJsonAsync("/books", book);
+        _createdIsbns.Add(book.Isbn);
+
+        // Act
+        var result = await httpClient.GetAsync($"/books?searchTerm=Invalid");
+        var returnedBooks = await result.Content.ReadFromJsonAsync<List<Book>>();
+
+        // Assert
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+        returnedBooks.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task UpdateBook_UpdatesBook_WhenDataIsCorrect()
+    {
+        // Arrange
+        var httpClient = factory.CreateClient();
+        var book = GenerateBook();
+        await httpClient.PostAsJsonAsync("/books", book);
+        _createdIsbns.Add(book.Isbn);
+        book.Title = "Updated Title";
+
+        // Act
+        var result = await httpClient.PutAsJsonAsync($"/books/{book.Isbn}", book);
+        var updatedBook = await result.Content.ReadFromJsonAsync<Book>();
+
+        // Assert
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+        updatedBook.Should().BeEquivalentTo(book);
+    }
+
+    [Fact]
+    public async Task UpdateBook_Fails_WhenIsbnIsInvalid()
+    {
+        // Arrange
+        var httpClient = factory.CreateClient();
+        var book = GenerateBook();
+        await httpClient.PostAsJsonAsync("/books", book);
+        _createdIsbns.Add(book.Isbn);
+        book.Isbn = "Invalid ISBN";
+
+        // Act
+        var result = await httpClient.PutAsJsonAsync($"/books/{book.Isbn}", book);
+        var errors = await result.Content.ReadFromJsonAsync<IEnumerable<ValidationError>>();
+        var error = errors!.Single();
+
+        // Assert
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        error.PropertyName.Should().Be("Isbn");
+        error.ErrorMessage.Should().Be("Invalid ISBN format");
+    }
+
+    [Fact]
+    public async Task DeleteBook_DeletesBook_WhenBookExists()
+    {
+        // Arrange
+        var httpClient = factory.CreateClient();
+        var book = GenerateBook();
+        await httpClient.PostAsJsonAsync("/books", book);
+        _createdIsbns.Add(book.Isbn);
+
+        // Act
+        var result = await httpClient.DeleteAsync($"/books/{book.Isbn}");
+
+        // Assert
+        result.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+
+    [Fact]
+    public async Task DeleteBook_ReturnsNotFound_WhenBookDoesNotExist()
+    {
+        // Arrange
+        var httpClient = factory.CreateClient();
+        var isbn = GenerateIsbn();
+
+        // Act
+        var result = await httpClient.DeleteAsync($"/books/{isbn}");
+
+        // Assert
+        result.StatusCode.Should().Be(HttpStatusCode.NotFound);
     }
 
     private Book GenerateBook(string title = "Test Book")
@@ -179,9 +266,9 @@ public class LibraryEndpointsTests(WebApplicationFactory<IApiMarker> factory)
     public async Task DisposeAsync()
     {
         var httpClient = factory.CreateClient();
-        foreach (var isbn in _createdIsbns)
+        foreach (var createdIsbn in _createdIsbns)
         {
-            await httpClient.DeleteAsync($"/books/{isbn}");
+            await httpClient.DeleteAsync($"/books/{createdIsbn}");
         }
     }
 }
